40b21cb01aaf46c63551f108bafe8fa118adfa70
Alexander Sattarov <zdarovyak-007@yandex.ru>
2024-07-26 02:03:15 +0300

    Implement replacing of original method that not works(

---
+++ LibOverrider.cs
@@ -0,0 +1,53 @@
+﻿using Jellyfin.Data.Enums;
+using MediaBrowser.Controller.Entities;
+using MediaBrowser.Controller.Library;
+using MediaBrowser.Model.Querying;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortRawNamePlugin
+{
+    public class LibOverrider
+    {
+        private readonly MethodInfo _method;
+        private readonly ILibraryManager _lm;
+
+        public LibOverrider(ILibraryManager lm, MethodInfo method)
+        {
+            _method = method;
+            _lm = lm;
+        }
+
+        public QueryResult<BaseItem> GetItemsResult(InternalItemsQuery query)
+        {
+            if (query.OrderBy.Count > 0)
+            {
+                var original = query.OrderBy;
+                var replasedOrderBy = new List<(ItemSortBy OrderBy, SortOrder SortOrder)>();
+                foreach (var order in original)
+                {
+                    var pair = (order.OrderBy, order.SortOrder);
+                    if (order.OrderBy == ItemSortBy.SortName)
+                    {
+                        pair.OrderBy = ItemSortBy.Name;
+                    }
+                    replasedOrderBy.Add(pair);
+                }
+                query.OrderBy = replasedOrderBy;
+                var resRaw = _method.Invoke(_lm, [query]); // _lm.GetItemsResult(query);
+                var res = (QueryResult<BaseItem>)resRaw;
+                query.OrderBy = original;
+
+                var sorted = new QueryResult<BaseItem>(_lm.Sort(res.Items, query.User, query.OrderBy).ToList());
+                sorted.StartIndex = res.StartIndex;
+                sorted.TotalRecordCount = res.TotalRecordCount;
+                return sorted;
+            }
+            return _lm.GetItemsResult(query);
+        }
+    }
+}
--- Plugin.cs
+++ Plugin.cs
@@ -1,95 +1,27 @@
 ﻿using Jellyfin.Data.Enums;
 using MediaBrowser.Common.Configuration;
 using MediaBrowser.Common.Plugins;
+using MediaBrowser.Controller;
 using MediaBrowser.Controller.Configuration;
 using MediaBrowser.Controller.Entities;
+using MediaBrowser.Controller.Library;
+using MediaBrowser.Controller.Plugins;
 using MediaBrowser.Controller.Sorting;
 using MediaBrowser.Model.Serialization;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using SortRawNamePlugin.Configuration;
 
 namespace SortRawNamePlugin
 {
-    public class Plugin : BasePlugin<PluginConfiguration>, IBaseItemComparer
+    public class Plugin : BasePlugin<PluginConfiguration>
     {
-        private readonly ILogger<Plugin> _logger;
 
-        public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer, ILogger<Plugin> logger) : base(applicationPaths, xmlSerializer)
+        public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer) : base(applicationPaths, xmlSerializer)
         {
-            _logger = logger;
         }
 
         public override string Name => "SortByRawName";
         public override Guid Id => Guid.Parse("BDBFD97C-CCD9-4226-B28C-B272F5A1617C");
-
-        public ItemSortBy Type => ItemSortBy.SortName;
-
-        public int Compare(BaseItem? x, BaseItem? y)
-        {
-            ArgumentNullException.ThrowIfNull(x);
-            ArgumentNullException.ThrowIfNull(y);
-            var first = GetSortName(x);
-            var second = GetSortName(y);
-            _logger.LogInformation("Comparing: '{First}' vs '{Second}'", first, second);
-
-            return string.Compare(first, second, StringComparison.OrdinalIgnoreCase);
-        }
-
-        internal static string GetSortName(BaseItem item)
-        {
-            if (!string.IsNullOrEmpty(item.ForcedSortName)) {
-                // Need the ToLower because that's what CreateSortName does
-                return item.ForcedSortName.ToLowerInvariant();
-            }
-            else
-            {
-                return CreateSortName(item);
-            }
-        }
-
-        internal static string CreateSortName(BaseItem item)
-        {
-            if (item.Name is null)
-            {
-                return null; // some items may not have name filled in properly
-            }
-
-            if (!item.EnableAlphaNumericSorting)
-            {
-                return item.Name.TrimStart();
-            }
-
-            var sortable = item.Name.Trim().ToLowerInvariant();
-
-            foreach (var search in BaseItem.ConfigurationManager.Configuration.SortRemoveWords)
-            {
-                // Remove from beginning if a space follows
-                if (sortable.StartsWith(search + " ", StringComparison.Ordinal))
-                {
-                    sortable = sortable.Remove(0, search.Length + 1);
-                }
-
-                // Remove from middle if surrounded by spaces
-                sortable = sortable.Replace(" " + search + " ", " ", StringComparison.Ordinal);
-
-                // Remove from end if followed by a space
-                if (sortable.EndsWith(" " + search, StringComparison.Ordinal))
-                {
-                    sortable = sortable.Remove(sortable.Length - (search.Length + 1));
-                }
-            }
-
-            foreach (var removeChar in BaseItem.ConfigurationManager.Configuration.SortRemoveCharacters)
-            {
-                sortable = sortable.Replace(removeChar, string.Empty, StringComparison.Ordinal);
-            }
-
-            foreach (var replaceChar in BaseItem.ConfigurationManager.Configuration.SortReplaceCharacters)
-            {
-                sortable = sortable.Replace(replaceChar, " ", StringComparison.Ordinal);
-            }
-
-            return sortable;
-        }
     }
 }
